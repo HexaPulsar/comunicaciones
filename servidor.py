5e106f7c683250b95881a8baa940f923070ce0b3
MakitaTriste <magdalenadelafuente@ug.uchile.cl>
2022-05-04 11:15:57 -0400

    "alram

--- servidor.py
+++ servidor.py
@@ -40,7 +40,7 @@ class Client_thread(threading.Thread):
         self.chatear = threading.Event()
     #funcion run es el punto de partida del objeto thread que se creo
     #esta funcion se gatilla haciendo .start()
-    def run(self,signal):
+    def run(self):
         while len(self.name) != 9:
             self.socket.sendall(bytes('Largo de rut no válido, intente denuevo', 'utf-8'))
             self.name = self.socket.recv(1024).decode('utf-8') 
